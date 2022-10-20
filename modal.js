526079096fe8a2ff75513871b7c72c4becd19c09
Діма Якименко <akimenkodima154@gmail.com>
2022-10-20 13:11:36 +0100

    master

--- modal.js
+++ modal.js
@@ -25,3 +25,17 @@
     }
     
     })();
+
+    (() => {
+        document
+            .querySelector('.js-speaker-form')
+            .addEventListener('submit', e => {
+                e.preventDefault();
+
+                new FormData(e.currentTarget).forEach((value, name) =>
+                    console.log(`${name}: ${value}`),
+                );
+
+                e.currentTarget.reset();
+            });
+    })();
